e1bc77fb347af27d36c5f00bc483f189ca60be3e
Tanny2709 <123792042+Tanny2709@users.noreply.github.com>
2025-09-17 17:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-//complete this code
-class Animal {}
+// Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+// Dog class
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Cat class
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Example usage
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // The Siamese makes a sound
+myCat.purr();      // purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // The Golden Retriever makes a sound
+myDog.bark();      // woof
 
 // Do not change the code below this line
 window.Animal = Animal;
